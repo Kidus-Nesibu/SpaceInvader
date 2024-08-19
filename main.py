a740eeccb21958e5eb5e105afb08d191fbc4306c
Kidus-Nesibu <Knesibu3@gmail.com>
2024-08-19 15:00:07 +0300

    added a bullet and movement functionality for the bullet

--- main.py
+++ main.py
@@ -30,11 +30,24 @@ enemeyY = random.randint(50, 150)
 enemeyX_change = 3
 enemeyY_change = 40
 
+#Bullet
+bulletimg = pygame.image.load('bullet.png')
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 10
+bullet_state = "ready"
+
 def player(x, y):
     # blit means draw
     screen.blit(playerimg,(x, y))
 def enemey(x, y):
     screen.blit(enemeyimg, (x, y))
+
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletimg, (x + 16, y + 10))
 
 #Game loop
 running = True
@@ -48,6 +61,8 @@ while running:
                 playerX_change -= 5
             elif event.key == pygame.K_RIGHT:
                 playerX_change += 5
+            elif event.key == pygame.K_SPACE:
+                fire_bullet(playerX, bulletY)
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -71,6 +86,11 @@ while running:
     elif enemeyX >= 736:
         enemeyX_change = -3
         enemeyY += enemeyY_change
+
+    #Bullet movment
+    if bullet_state is "fire":
+        fire_bullet(playerX, bulletY)
+        bulletY -= bulletY_change
 
     print(playerX)
     player(playerX, playerY)
